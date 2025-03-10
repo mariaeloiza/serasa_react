9935d9378b39b742e50adb68fa3cf32ccc286e20
mariaeloiza <elooiza1@gmail.com>
2025-03-10 15:32:33 -0300

    Create page.js

---
+++ app/quiz/page.js
@@ -0,0 +1,24 @@
+function Quiz() {
+    return ( 
+        <div>
+
+            <h1>Quiz Conradito</h1>
+            <p>Assinale a opção correta!</p>
+
+            <hr/>
+
+            <h2>Qual a cor do cavalo branco de Napoleãum?</h2>
+            <label><input type="radio" /> Preto </label>
+            <br/>
+            <label><input type="radio" /> Azul </label>
+            <br/>
+            <label><input type="radio" /> Branco </label>
+            <br/>
+            <label><input type="radio" /> Transparente </label>
+            <br/>
+
+        </div>
+    );
+}
+
+export default Quiz;
